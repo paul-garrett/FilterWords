d7b629680988aa5cd6752f922414d8290b9dfcca
Paul Garrett <89760819+paul-garrett@users.noreply.github.com>
2023-12-13 14:36:24 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) {
+		// System.out.println("Hello world!");
+    // Need input paths
+    String inputFile = "words.txt";
+    // Read input
+    try {
+      FileReader fileReader = new fileReader("words.txt");
+         Scanner scan = new Scanner(file);
+         while (scan.hasNextLine()) {
+           System.out.println(scan.nextLine());
+      }
+    }
+    catch (FileNotFoundException e) {
+      File file = new File("words.txt");
+      try (FileWriter bw = new FileWriter(file, true)) {
+        
+      }
+    // Write
+    // Start at first index 
+      string i = a;
+      while ()
+    // Sort characters between a-z lowercase
+    // + one or more characters
+    // increment through the entire word 
+	}
+}
